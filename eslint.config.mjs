065bb7ea6bf4232b98a79af753c7b9ec2a7fb5e7
Sanju Singh <119477810+sanjurajsingh@users.noreply.github.com>
2026-02-02 17:58:39 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,8 +13,8 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-unused-vars": "off" [
-        "error",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
         {
           argsIgnorePattern: "^_",
           varsIgnorePattern: "^_",
