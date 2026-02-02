c23d52408e40811d5a1edcbf4d04505676d0cc83
Sanju Singh <119477810+sanjurajsingh@users.noreply.github.com>
2026-02-03 00:23:18 +0530

    Update minikit.config.ts

--- minikit.config.ts
+++ minikit.config.ts
@@ -1,2 +1,12 @@
 export const minikitConfig = {
-  
+  export const minikitConfig = {
+  accountAssociation: {
+    header: "eyJmaWQiOjQ5NTk0NywidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDAzQkY5NzljNmYyNTI2ZkI3MzZhNmZkN2YwNzRDYTBjMGIxQ0I3MUUifQ",
+    payload: "eyJkb21haW4iOiJiYXNlZ2FzYnVkZHkudmVyY2VsLmFwcCJ9",
+    signature: "SGGm85VDVL+cljWQtuYbmu41iqglP36hufvJnFfGFqRfnmPYtnNqH1tROBH+69Un9arw94+PekH/BzUULmG75Rs="
+  },
+  frame: {
+    version: "vNext"
+  }
+};
+
